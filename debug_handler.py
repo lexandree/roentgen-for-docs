0d3693e735ad379417d8fa9565f35b1b439d6e1e
lexandree <andreev.al@gmail.com>
2026-03-18 01:38:08 +0100

    chore: move debug python scripts to tmp directory

--- debug_handler.py
+++
@@ -1,4 +0,0 @@
-from llama_cpp.llama_chat_format import Llava15ChatHandler
-import inspect
-
-print(inspect.getsource(Llava15ChatHandler.__call__))
--- test_llava_format.py
+++
@@ -1,6 +0,0 @@
-import inspect
-try:
-    from llama_cpp.llama_chat_format import Llava15ChatHandler
-    print(inspect.getsource(Llava15ChatHandler))
-except Exception as e:
-    print(e)
